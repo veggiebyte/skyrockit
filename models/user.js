7b6a7d1f611062f1d4d2b25d42f9cbe05f51f0c5
“CindyS” <veggiebyte@yahoo.com>
2025-12-09 15:25:29 -0800

    Build the Homepage

--- models/user.js
+++ models/user.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +30,8 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+
+  applications: [applicationSchema], 
 });
 
 const User = mongoose.model('User', userSchema);
