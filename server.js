0edc2ca2371b7394247a606bf2e0e8c97e68dcf8
“CindyS” <veggiebyte@yahoo.com>
2025-12-09 20:07:48 -0800

    Build and Use Application Server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 
 const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -40,8 +41,10 @@ app.get('/', (req, res) => {
   });
 });
 
+
 app.use('/auth', authController);
 app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController); // New!
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
