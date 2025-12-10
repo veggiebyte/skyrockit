662c44d0d896c387a4ca72250f4ff94018300d5e
“CindyS” <veggiebyte@yahoo.com>
2025-12-09 20:03:01 -0800

    Add Route Middleware

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -28,6 +32,8 @@ app.use(
   })
 );
 
+app.use(passUserToView); 
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -35,6 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
