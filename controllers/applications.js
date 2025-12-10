37d80336c7121fa8b75f1e4367a4fafd21030194
“CindyS” <veggiebyte@yahoo.com>
2025-12-10 01:11:38 -0800

    Build Create Functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-module.exports = router;
-
 router.get('/', async (req, res) => {
   try {
     res.render('applications/index.ejs');
@@ -17,3 +15,23 @@ router.get('/', async (req, res) => {
 router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
+
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
