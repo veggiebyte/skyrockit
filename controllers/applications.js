1b0a7ee4dba9ddbd8f628f417d5bff2ceb102a10
“CindyS” <veggiebyte@yahoo.com>
2025-12-09 21:15:04 -0800

    Building the applications index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,5 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-
 module.exports = router;
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('applications/index.ejs');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
--- server.js
+++ server.js
@@ -24,7 +24,6 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -36,11 +35,15 @@ app.use(
 app.use(passUserToView); 
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
+  }
 });
-
 
 app.use('/auth', authController);
 app.use(isSignedIn);
