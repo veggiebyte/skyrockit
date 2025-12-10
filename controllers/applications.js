c4bef91195a89113d5c7eb746a87e4b623ab027f
“CindyS” <veggiebyte@yahoo.com>
2025-12-10 10:25:31 -0800

    Build Delete Functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,6 +3,24 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+router.delete('/:applicationId', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Use the Mongoose .deleteOne() method to delete
+    // an application using the id supplied from req.params
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
     // Look up the user from req.session
